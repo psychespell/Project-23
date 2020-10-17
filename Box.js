63b4fc2aaefa4c9952c047f586a47916acb992a1
psychespell <50533970+psychespell@users.noreply.github.com>
2020-10-17 17:18:17 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,15 @@
+class Box{
+    constructor(x, y, width, height) {
+        var options = {
+            'friction':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+        }
+
+    display(){
+        bottom.fill = "red";
+    }
+}
